4b3ee30e5430f8a292e2fbd12eaa05521a08f3c8
PraveenNegi1 <praveennegi700@gmail.com>
2026-05-21 17:26:53 +0530

    done

--- components/Map.jsx
+++ components/Map.jsx
@@ -20,7 +20,6 @@ export default function GoogleIndiaMap() {
   return (
     <section className="relative w-full py-20 bg-linear-to-b from-sky-100 to-white overflow-hidden">
 
-      {/* HEADING */}
       <div className="max-w-7xl mx-auto px-4 text-center mb-10">
 
         <h2 className="text-3xl  font-bold text-gray-900">
